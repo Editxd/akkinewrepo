e6dd2d155039fc89d77c0f188fed3436c1430b93
ojha111 <152144787+ojha111@users.noreply.github.com>
2023-12-05 20:08:53 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+## Modified Text Pyro Bot by @noob_creator
+
+## Deploy To Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/Adity012/leechxankush">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
+
+  
+## Fix
+ #File Directory 
+ #Ban bypass 
+ #Bugs In codes
+
+## Update
+ #Can Download One Link also
---
+++ p_bar.py
@@ -0,0 +1,119 @@
+#  MIT License
+#
+#  Copyright (c) 2019-present Dan <https://github.com/delivrance>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE
+
+import time
+import math
+import os
+from pyrogram.errors import FloodWait
+
+class Timer:
+    def __init__(self, time_between=2):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
+
+from datetime import datetime,timedelta
+
+def hrb(value, digits= 2, delim= "", postfix=""):
+    """Return a human-readable file size.
+    """
+    if value is None:
+        return None
+    chosen_unit = "B"
+    for unit in ("KiB", "MiB", "GiB", "TiB"):
+        if value > 1000:
+            value /= 1024
+            chosen_unit = unit
+        else:
+            break
+    return f"{value:.{digits}f}" + delim + chosen_unit + postfix
+
+def hrt(seconds, precision = 0):
+    """Return a human-readable time delta as a string.
+    """
+    pieces = []
+    value = timedelta(seconds=seconds)
+    
+
+    if value.days:
+        pieces.append(f"{value.days}d")
+
+    seconds = value.seconds
+
+    if seconds >= 3600:
+        hours = int(seconds / 3600)
+        pieces.append(f"{hours}h")
+        seconds -= hours * 3600
+
+    if seconds >= 60:
+        minutes = int(seconds / 60)
+        pieces.append(f"{minutes}m")
+        seconds -= minutes * 60
+
+    if seconds > 0 or not pieces:
+        pieces.append(f"{seconds}s")
+
+    if not precision:
+        return "".join(pieces)
+
+    return "".join(pieces[:precision])
+
+
+
+timer = Timer()
+
+async def progress_bar(current, total, reply, start):
+    if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            remaining_bytes = total - current
+            if speed > 0:
+                eta_seconds = remaining_bytes / speed
+                eta = hrt(eta_seconds, precision=1)
+            else:
+                eta = "-"
+            sp = str(hrb(speed)) + "/s"
+            tot = hrb(total)
+            cur = hrb(current)
+            
+            # Calculate progress bar dots
+            bar_length = 18
+            completed_length = int(current * bar_length / total)
+            remaining_length = bar_length - completed_length
+            progress_bar = "■" * completed_length + "□" * remaining_length
+            
+            try:
+                await reply.edit(f"`Bot Made By: LPRPDV\n\n[{progress_bar}] \n\n➠𝚂𝚒𝚣𝚎 :- {cur} / {tot} \n➠𝙿𝚛𝚘𝚌𝚎𝚜𝚜 :- {perc}\n➠𝚂𝚙𝚎𝚎𝚍 :- {sp} \n➠𝙴𝚃𝙰 :- {eta}\n\n𝐁𝐎𝐓 𝐁𝐘» @LPRPDV")
+            except FloodWait as e:
+                time.sleep(e.x)
